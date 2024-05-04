85d4915c864a25ce113c2b6e6147c45bd46d9255
Popeydanielmate <14269@coderacademy.edu.au>
2024-05-04 21:17:36 +1000

    feat: refactor for trycatch and nodemon script

--- index.js
+++ index.js
@@ -13,12 +13,20 @@ let promptInstance = promptPackage({sigint: true});
 //
 //var n = prompt('What number of Pokemon do you want to see?');
 
+function app() {
 
-let userWantsToExit = false;
+    let userWantsToExit = false;
 
 do {
 
     let n = parseFloat(prompt("What number of Pokemon do you want to see?"));
+
+    console.log(typeof(n));
+    console.log("Input is not a number:" + Number.isNaN(n));
+
+    if (Number.isNaN(n)){
+        throw new Error("User did not enter a number!");
+    }
     console.log("You entered: " + n);
 
     let userInputToExit = prompt("Would you like to try again?");
@@ -28,7 +36,15 @@ do {
     } else {
         userWantsToExit = true;
     }
-} while (userWantsToExit == false);
+} while (userWantsToExit == false); 
+}
+
+try {
+    app();
+} catch (error) {
+    console.log("Gracefully shutting down...");
+    console.log(error.message);
+}
 
 //console.log("User entered: " + n);
 //console.log(typeof(n));
@@ -36,10 +52,10 @@ do {
 //let nAsNumber = parseFloat(n);
 //console.log(typeof(nAsNumber));
 
-console.log(process.env.ENVIRONMENT_MESSAGE);
+/*console.log(process.env.ENVIRONMENT_MESSAGE);
 
 console.log("The terminal app is running");
 
 pokemonPrinterFile.pokemonPrinter();
 
-console.log("Bye bye, terminal app is finished");
+console.log("Bye bye, terminal app is finished");*/
