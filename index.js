c47d515c70c94613185e60d2d92b9ceabdd35a05
Popeydanielmate <14269@coderacademy.edu.au>
2024-05-04 20:45:29 +1000

    feat: repeatable user input

--- index.js
+++ index.js
@@ -2,6 +2,40 @@ require('dotenv').config();
 
 const pokemonPrinterFile = require("./pokemonPrinter");
 
+// style 1 for setting up prompt-sync
+var prompt = require('prompt-sync')({sigint: true});
+
+// style 2 for setting up prompt-sync
+let promptPackage = require('prompt-sync');
+let promptInstance = promptPackage({sigint: true});
+//
+// get input from the user.
+//
+//var n = prompt('What number of Pokemon do you want to see?');
+
+
+let userWantsToExit = false;
+
+do {
+
+    let n = parseFloat(prompt("What number of Pokemon do you want to see?"));
+    console.log("You entered: " + n);
+
+    let userInputToExit = prompt("Would you like to try again?");
+
+    if (userInputToExit == "y"){
+        userWantsToExit = false;
+    } else {
+        userWantsToExit = true;
+    }
+} while (userWantsToExit == false);
+
+//console.log("User entered: " + n);
+//console.log(typeof(n));
+
+//let nAsNumber = parseFloat(n);
+//console.log(typeof(nAsNumber));
+
 console.log(process.env.ENVIRONMENT_MESSAGE);
 
 console.log("The terminal app is running");
