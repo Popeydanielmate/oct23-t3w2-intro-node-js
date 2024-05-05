eded9a5699d4d497496048d7e8519be78cb59202
Popeydanielmate <14269@coderacademy.edu.au>
2024-05-05 13:08:16 +1000

    feat: custom error implemented

---
+++ customError.js
@@ -0,0 +1,14 @@
+class CustomFancyError extends Error {
+    constructor(message){
+        super(message)
+        this.secretMessage = "Bananas!";
+    }
+
+    toJSON(){
+        return{
+            secretMessage: this.secretMessage
+        }
+    }
+}
+
+module.exports = { CustomFancyError }
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 
+const { CustomFancyError } = require('./customError');
 const pokemonPrinterFile = require("./pokemonPrinter");
 const {pokemonNameFromNumber} = require('./pokemonPrinter');
 
@@ -26,7 +27,8 @@ do {
     //console.log("Input is not a number:" + Number.isNaN(n));
 
     if (Number.isNaN(n)){
-        throw new Error("User did not enter a number!");
+        //throw new Error("User did not enter a number!");
+        throw new CustomFancyError("Custom error for NaN!");
     }
 
     // Give number to pokemon package and get result
@@ -36,17 +38,17 @@ do {
 		console.log(`Your Pokemon is ${pokemonName}! How exciting!`);
     } catch (error) {
         console.log("Try a number between 1 and 1025!");
-    }
-
-    //console.log("You entered: " + n);
-
-    let userInputToExit = prompt("Would you like to try again?");
+    } finally {
+        let userInputToExit = prompt("Would you like to try again?");
 
     if (userInputToExit == "y"){
         userWantsToExit = false;
     } else {
         userWantsToExit = true;
     }
+    }
+
+    //console.log("You entered: " + n);
 } while (userWantsToExit == false); 
 }
 
@@ -55,6 +57,7 @@ try {
 } catch (error) {
     console.log("Gracefully shutting down...");
     console.log(error.message);
+    console.log(JSON.stringify(error));
 }
 
 //console.log("User entered: " + n);
